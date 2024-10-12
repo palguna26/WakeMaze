a77640746a8d15f5200aae4c491f4b0e0b40e3be
palguna26 <palgunashetty26@gmail.com>
2024-10-12 20:11:43 +0530

    WakeMaze:The future of alarms.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
